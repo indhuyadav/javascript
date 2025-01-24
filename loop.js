5e98f72db294e3f277cd757a81c3533b5bc3a882
indhuyadav <indhuy88@gmail.com>
2025-01-24 12:26:25 +0530

    sending files

---
+++ loop.js
@@ -0,0 +1,22 @@
+//looping statement
+//is used to execute/iterate 
+
+let i=1;
+while(i<=5){
+    console.log(i);
+    i++;
+}
+
+//do-while
+let a=4;
+do{
+    console.log(a);
+    a--;
+}
+while(a>=1)
+//4 3 2 1
+
+//for loop
+for(let i=1;i<=5;i++){
+    console.log(i);
+}
---
+++ switch.js
@@ -0,0 +1,29 @@
+var userinput = prompt("Enter the day: ").toLowerCase(); 
+
+switch (userinput) {
+    case "monday":
+    case "tuesday":
+        console.log("football");
+        break;
+        
+    case "wednesday":
+    case "thursday":
+        console.log("cricket");
+        break;
+        
+    case "friday":
+        console.log("volleyball");
+        break;
+        
+    case "saturday":
+        console.log("basketball");
+        break;
+        
+    case "sunday":
+        console.log("holiday");
+        break;
+        
+    default:
+        console.log("Enter a valid day");
+        break;
+}
